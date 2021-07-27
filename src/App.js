5e9be874e4b52c0fe3c5c0a345a4888b1f921ff9
LúcioBJ <lbjunq@hotmail.com>
2021-07-27 15:41:22 -0300

    Adicionado os requisitos um, dois, tres e quatro

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
 import './App.css';
+import data from './data'
 
 function App() {
+  const movies = data;
   return (
     <div className="App">
       <Header />
+      <MovieList movies={ movies }/>
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,40 @@
-// implement SearchBar component here
+import { set } from 'lodash';
+import { Components } from 'react';
+import { setSyntheticTrailingComments } from 'typescript';
+
+class SearchBar extends Components {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      searchText: '',
+      onSearchTextChange: '',
+      bookmarkedOnly: false,
+      onBookmarkedChange: '',
+      selectedGenre: '',
+      onSelectedGenreChange: '',
+    };
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.name]: event.target.value,
+    });
+  }
+
+  render() {
+    <div>
+      <form data-testid="search-bar-form">
+        Search Bar
+        <label data-testid="text-input-label">Inclui o texto:
+          <input data-testid="text-input" type='search' value={ searchText } onChange={ onSearchTextChange } />
+        </label>
+        <label data-testid="checkbox-input-label">Mostrar somente favoritos
+          <input data-testid="checkbox-input" type='checkbox' value={ bookmarkedOnly } onChange={ onBookmarkedChange } />
+        </label>
+      </form>
+    </div>
+  }
+}
